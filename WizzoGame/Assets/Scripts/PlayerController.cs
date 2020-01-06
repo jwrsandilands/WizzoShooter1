beb0e810a8a13e4be13bece1bf448100b7f56dac
jwrsandilands <55706784+jwrsandilands@users.noreply.github.com>
2020-01-06 14:44:55 +0000

    Changed file and walk in void
    added the game file to let the player walk around the void.

---
+++ WizzoGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    Rigidbody2D rb2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        rb2D.velocity = new Vector2(x, y) * speed;
+        rb2D.angularVelocity = 0.0f;
+    }
+}
